bce31b1c03ed585c2d00d447a17a91e05196309f
최종천 <144365951+choijongchun@users.noreply.github.com>
2023-09-19 16:49:03 +0900

    Add files via upload

---
+++ Homework01.java
@@ -0,0 +1,48 @@
+package Homework;
+
+public class Homework01 {
+	public static void main(String[] args) {
+		for(int i = 0; i < 1; i++) {
+			// i = 0 j = 0 print: *
+			for(int j = 0; 
+					j < i+1; j++) {
+				System.out.print("   *");
+			}
+			
+			System.out.println();
+			
+			
+		}
+		for(int i = 0; i < 1; i++) {
+			// i = 0 j = 0 print: *
+			for(int j = 0; 
+					j < i+1; j++) {
+				System.out.print("  **");
+			}
+			
+			System.out.println();
+		}
+			for(int i = 0; i < 1; i++) {
+				// i = 0 j = 0 print: *
+				for(int j = 0; 
+						j < i+1; j++) {
+					System.out.print(" ***");
+				}
+				
+				System.out.println();
+			}
+			for(int i = 0; i < 1; i++) {
+					// i = 0 j = 0 print: *
+					for(int j = 0; 
+							j < i+1; j++) {
+						System.out.print("****");
+					}
+					
+					System.out.println();
+					
+					
+				}
+			}
+			
+		}
+	
---
+++ Homework03.java
@@ -0,0 +1,70 @@
+package Homework;
+
+public class Homework03 {
+	public static void main(String[] args) {
+		for(int i = 0; i < 1; i++) {
+			// i = 0 j = 0 print: *
+			for(int j = 0; 
+					j < i+1; j++) {
+				System.out.print("   *");
+			}
+			
+			System.out.println();
+	}
+		for(int i = 0; i < 1; i++) {
+			// i = 0 j = 0 print: *
+			for(int j = 0; 
+					j < i+1; j++) {
+				System.out.print("  ***");
+			}
+			
+			System.out.println();
+		}
+		for(int i = 0; i < 1; i++) {
+			// i = 0 j = 0 print: *
+			for(int j = 0; 
+					j < i+1; j++) {
+				System.out.print(" *****");
+			}
+			
+			System.out.println();
+		}
+		
+		for(int i = 0; i < 1; i++) {
+			// i = 0 j = 0 print: *
+			for(int j = 0; 
+					j < i+1; j++) {
+				System.out.print("*******");
+			}
+			
+			System.out.println();
+}
+		for(int i = 0; i < 1; i++) {
+			// i = 0 j = 0 print: *
+			for(int j = 0; 
+					j < i+1; j++) {
+				System.out.print(" *****");
+			}
+			
+			System.out.println();
+		}
+		for(int i = 0; i < 1; i++) {
+			// i = 0 j = 0 print: *
+			for(int j = 0; 
+					j < i+1; j++) {
+				System.out.print("  ***");
+			}
+			
+			System.out.println();
+		}
+		for(int i = 0; i < 1; i++) {
+			// i = 0 j = 0 print: *
+			for(int j = 0; 
+					j < i+1; j++) {
+				System.out.print("   *");
+			}
+			
+			System.out.println();
+		}
+}
+}
